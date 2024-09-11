c4da0fe24c1cc14613c06c8a229e470b629f215c
MdShohagHossen <mdshohaghossen29@gmail.com>
2024-09-11 10:56:16 +0600

    Fixed the problem

--- src/Pages/Doctors/CheckoutSuccess.jsx
+++ src/Pages/Doctors/CheckoutSuccess.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 const CheckoutSuccess = () => {
   return (
-    <div className="bg-[#E6F1FF] md:h-[90vh] flex items-center justify-center">
-      <div className="card p-[30px] w-[40%] flex flex-col items-center ">
+    <div className="bg-[#E6F1FF] md:h-[90vh] md:px-0 px-[20px] md:flex md:items-center md:justify-center">
+      <div className="card p-[30px] md:w-[40%] flex flex-col items-center ">
         <IoMdCloudDone className="text-[#002570] text-[50px]" />
         <div className="text-center">
           <h3 className="text-[#002570] text-[25px] font-bold">

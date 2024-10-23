71694e2ae0c8ff02a24567086d1b94b1bd2d5935
MdShohagHossen <mdshohaghossen29@gmail.com>
2024-10-23 12:16:22 +0600

    Top button add

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Routers from "../routes/Routers";
+import GoTop from "../components/GoTop/GoTop";
 
 const Layout = () => {
   return (
     <>
       <Header />
+      <GoTop />
       <main>
         <Routers />
       </main>
